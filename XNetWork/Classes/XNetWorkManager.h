10c0d170234f588cde470b785cfde68da494a1a1
Xu <972510000@qq.com>
2020-04-25 12:57:23 +0800

    [update]manager

---
+++ XNetWork/Classes/XNetWorkManager.h
@@ -0,0 +1,17 @@
+//
+//  XNetWorkManager.h
+//  XNetWork_Example
+//
+//  Created by WenQiangXu on 2020/4/25.
+//  Copyright © 2020 972510000@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XNetWorkManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XNetWork/Classes/XNetWorkManager.m
@@ -0,0 +1,13 @@
+//
+//  XNetWorkManager.m
+//  XNetWork_Example
+//
+//  Created by WenQiangXu on 2020/4/25.
+//  Copyright © 2020 972510000@qq.com. All rights reserved.
+//
+
+#import "XNetWorkManager.h"
+
+@implementation XNetWorkManager
+
+@end
